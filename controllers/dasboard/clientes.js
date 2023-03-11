5888fdeb488bdbdec91a1b06bbb94538f7f8b8a4
20210479 <87909508+20210479@users.noreply.github.com>
2023-03-11 17:05:30 -0600

    Update clientes.js

--- controllers/dasboard/clientes.js
+++ controllers/dasboard/clientes.js
@@ -9,16 +9,11 @@ function openCreate() {
         html:
             '<div class="row"> <div class="col"><input id="swal-input1" placeholder="Nombres" class="swal2-input"></div> <div class="col"><input id="swal-input2" placeholder="Apellidos" class="swal2-input"></div></div>' +
 
-            '<div class="row flex-nowrap"><div class="col"><br><br><img src="https://img.icons8.com/sf-black-filled/32/null/overview-pages-2.png"/><input id="swal-input3" placeholder="Descripción" class="swal2-input" ></div><div class="col"><br><br><img src="https://img.icons8.com/ios-filled/30/null/idea--v1.png"/><input id="swal-input4" placeholder="Cantidad" class="swal2-input"> </div></div>' +
+            '<div class="row"><div class="col"><input id="iemail" type="email" placeholder="Correo" class="swal2-input" ></div><div class="col"><input id="swal-input4" placeholder="Cantidad" class="swal2-input"> </div></div>' +                            
 
             '<div class="row"><div class="col"><br><br><select class="form-select" aria-label="Categorias"><option selected>Categorias</option><option value="1">Refrigeradores</option><option value="2">Cocinas</option><option value="3">Lavadoras</option></select></div><div class="col"><br><br><select class="form-select" aria-label="Marcas"><option selected>Marcas</option><option value="1">Sony</option><option value="2">Samsung</option><option value="3">Edragas</option></select> </div></div>' +
 
-            '<br><select class="form-select" aria-label="Estado"><option selected>Estado</option><option value="1">Existente</option><option value="2">Inexistente</option>',
-        input: 'file',
-        inputAttributes: {
-            'accept': 'image/*',
-            'aria-label': 'Seleccionar Imagen del producto'
-        },
+            '<br><select class="form-select" aria-label="Estado"><option selected>Estado</option><option value="1">Existente</option><option value="2">Inexistente</option>',               
         showconfirmButton: true,
         preConfirm: () => {
             return [
