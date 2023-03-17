98bf6c17866463b12e61975eafd5cc0510e3cba4
Ellervruck <125489356+Elervruck@users.noreply.github.com>
2023-03-17 09:33:26 -0600

    se comentareo el codigo

--- controllers/public/nav-login.js
+++ controllers/public/nav-login.js
@@ -1,10 +1,10 @@
-
-
-
+/* Se crea una constante que mande a llamar al header */
 const header = document.querySelector('header')
+/* Se crea una constante que mande a llamar al Footer */
 const footer = document.querySelector('footer')
 const logo_img = document.querySelector('.logo-img')
 
+/* Se manda al header codigo para usarlo en el html */
 header.innerHTML = `
         <a href="" class="logo mb-0 d-none d-lg-inline-block"><img src="../../resources/images/logo.png" alt="" class="logo-img"></a>
         <nav class="navegation">
@@ -78,6 +78,7 @@ header.innerHTML = `
         </div>
     `;
 
+/* Se manda al footer codigo para usarlo en el html */
 footer.innerHTML = `
 <div class="d-flex flex-row justify-content-around" id="footer-content">
     <div class="py-3">
@@ -108,11 +109,15 @@ footer.innerHTML = `
 </div>
 `;
 
+ 
 var overlay = document.querySelector('.overlay')
+
+/* se manda a llamar un boton el cual al escucha el evento click se da al overlay la clase active */
 window.document.getElementById('btn-login').addEventListener('click', function () {
     overlay.classList.add('active')
 })
 
+/* se manda a llamar un boton el cual al escucha el evento click le quita al overlay la clase active */
 var btn_close = document.getElementById('btn-close-login')
 btn_close.addEventListener('click', function () {
     overlay.classList.remove('active')
@@ -120,6 +125,7 @@ btn_close.addEventListener('click', function () {
 
 var nav = document.getElementById('nav-info')
 
+/* se crea una funcion que al detectar el scroll en la ventana, y si este es mayor a 0 añade la clase active añ header y al nav y cuando sino la remueve */
 window.onscroll = function () {
     var y = window.scrollY
     if (y != 0) {
