2e27ac8f56685ec919e80e492658c9d0443c9353
Ellervruck <125489356+Elervruck@users.noreply.github.com>
2023-03-22 09:13:22 -0600

    cambios

--- controllers/public/alerts.js
+++ controllers/public/alerts.js
@@ -23,3 +23,14 @@ function Pedido() {
         }
     })
 }
+
+function confirmacion() {
+    Swal.fire({
+        icon: 'success',
+        text: 'Compra realizada exitosamente',
+        footer: '<a href="../public/productos.html">Seguir comprando?</a>',
+        background: '#fff8eb',
+        color: '#584342',
+        button: '#fff8eb'
+    })
+}
