b30cd87daaf59b6260260e545b7e118e571120e9
20210479 <20210479@ricaldone.edu.sv>
2023-08-17 09:38:44 -0600

    reporte de stock

--- helpers/format/report.js
+++ helpers/format/report.js
@@ -4,7 +4,7 @@ module.exports = {
     border: '10mm',
     header: {
         height: '15mm',
-        contents: '<h4 style=" color: red;font-size:20;font-weight:800;text-align:center;">PRODUCTOS DE CATEGORÍA</h4>'
+        contents: '<h4 style=" color: red;font-size:20;font-weight:800;text-align:center;">Reporte de cambios de Stock</h4>'
     },
     footer: {
         height: '20mm',
