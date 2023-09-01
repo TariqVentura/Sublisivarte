b9ebb41246e50d871d91f1f0b2e556a0a2362817
20210208 <20210208@users.noreply.github.com>
2023-09-01 08:22:52 -0600

    Modal newPassword

--- controllers/password.js
+++ controllers/password.js
@@ -1,4 +1,5 @@
 const FORM = document.getElementById('form')
+const FORM_P = document.getElementById('form_p')
 
 FORM.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -9,6 +10,25 @@ FORM.addEventListener('submit', function (e) {
             text: 'No se permiten campos vacios'
         })
     } else {
+        //axios post
+    }
+})
+
+FORM_P.addEventListener('submit', function (e) {
+    e.preventDefault()
+    if (!document.getElementById('password').value.trim() || !document.getElementById('password_confirm').value.trim()) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'No se permiten campos vacios'
+        })
+    } else if (!document.getElementById('password') != !document.getElementById('password_confirm')){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error...',
+            text: 'Las contraseñas no coinciden'
+        })
+    } else{
         //axios post
     }
 })
