757f29afd56cf199d0acf9dcaa9bc37760912f56
TariqVentura <20190052@ricaldone.edu.sv>
2023-09-03 09:32:02 -0600

    comentarios

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,26 +1,33 @@
+//llamamos al formulario
 const NEW_CATEGORY = document.getElementById('new-category')
 
+//funcion para enviar los datos a la api de categorias
 NEW_CATEGORY.addEventListener('submit', async (e) => {
+    //evitamos el proceso default
     e.preventDefault()
 
+    //capturamos los valores del formulario
     let category = document.getElementById('category').value
     let status = document.getElementById('inputGroupSelect01').value
 
+    //utilizamos la libreria de axios para enviar los datos a la api
     axios.post('http://localhost:443/api/categories', {
         categorie: category,
         status: status
     }, {
+        //definimos que utlizaremos body url encoded
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
     }).then(data => {
-        console.log(data)
+        //validamos si los datos son validos y si se han ingresado en la based para luego mostrar la alerta correspondiente
         if (data.data == true) {
             Swal.fire({
                 icon: 'success',
                 title: 'Proceso Exitoso',
                 text: 'Se ha creado la categoria exitosamente'
             }).then(() => {
+                //redirigimos a la pagina para visualizar los cambios
                 location.href = '/categorias'
             })
             
@@ -38,6 +45,7 @@ NEW_CATEGORY.addEventListener('submit', async (e) => {
             })
         }
     }).catch(err => {
+        //error generico
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
